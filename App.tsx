d594938373fd235315612985fcb03e72e24a03f7
ersillee <esiembuai@gmail.com>
2025-04-27 15:39:03 +0700

    Update

--- App.tsx
+++ App.tsx
@@ -1,99 +1,37 @@
-//1. import core component react-native
 import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  Image,
-  ScrollView,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
-//2. Buat component
-//   component adalah function yg return jsx
+import SplashScreen from './src/pages/SplashScreen';
+import SignUp from './src/pages/SignUp';
+import SignIn from './src/pages/SignIn';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import FlashMessage from 'react-native-flash-message';
+
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
-    //jsx
-    <>
-      <View style={styles.container}>
-        <Text style={styles.title}>Basic React Native</Text>
-      </View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <TextInput style={styles.input} placeholder="Enter your email" />
-        <TextInput style={styles.input} placeholder="Enter your password" />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Submit</Text>
-        </TouchableOpacity>
-        <Text style={styles.subTitle}>Image From URI</Text>
-        <Image
-          style={styles.img1}
-          source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{headerShown: false}}
         />
-        <Text style={styles.subTitle}>Image From Local Directory</Text>
-        <Image style={styles.img2} source={require('./assets/logo.png')} />
-        <Text style={styles.subTitle}>Image From Base64</Text>
-        <Image
-          style={styles.img1}
-          source={{
-            uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-          }}
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{headerShown: false}}
         />
-      </ScrollView>
-    </>
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+      <FlashMessage position="top" />
+    </NavigationContainer>
   );
 };
-//3. export component
-export default App;
 
-//4. Styling
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'red',
-    borderColor: 'black',
-    borderWidth: 5,
-    padding: 20,
-    margin: 20,
-    borderRadius: 20,
-  },
-  title: {
-    fontSize: 50,
-    fontWeight: '800',
-    color: 'yellow',
-    textAlign: 'center',
-  },
-  subTitle: {
-    fontSize: 30,
-    marginLeft: 20,
-    fontWeight: '500',
-    marginBottom: 10,
-  },
-  img1: {
-    height: 300,
-    width: 300,
-    marginLeft: 20,
-  },
-  img2: {
-    marginLeft: 20,
-    height: 233 / 3,
-    width: 1024 / 3,
-  },
-  input: {
-    borderColor: 'black',
-    borderWidth: 3,
-    margin: 20,
-    fontSize: 30,
-    borderRadius: 10,
-    padding: 15,
-  },
-  button: {
-    backgroundColor: 'blue',
-    margin: 20,
-    padding: 30,
-    borderRadius: 15,
-  },
-  buttonText: {
-    fontSize: 25,
-    textAlign: 'center',
-    color: 'white',
-  },
-});
+export default App;
--- index.js
+++ index.js
@@ -3,14 +3,15 @@
  */
 
 import {AppRegistry} from 'react-native';
-//import App from './App';
+import App from './App';
 //import Final from './Final';
-import SignUp from './SignUp';
+//import SignUp from './SignUp';
 //import Exercise4 from './Exercise4';
 //import Exercise3 from './Exercise3';
-//import Signin from './Signin';
+//import SignIn from './SignIn';
+//import SplashScreen from './SplashScreen';
 //import Registration from './Registration';
-import Exercise7 from './Exercise7';
+//import Exercise7 from './Exercise7';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => SignUp);
+AppRegistry.registerComponent(appName, () => App);
